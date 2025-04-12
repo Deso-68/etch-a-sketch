f3bd5aee7e3aecd7c6c5412cd0660a5df1679c82
Deso-68 <174117432+Deso-68@users.noreply.github.com.>
2025-04-12 15:49:09 +0800

    Add getRandomInt() that generates random int numbers

--- script.js
+++ script.js
@@ -48,8 +48,14 @@ function addHoverEvent() {
 
 function randomRgbValue() {
     let randomRgb = undefined;
-    
+
     `backgroud-color: rgba(${randomRgb})`;
+}
+
+function getRandomInt(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
 }
 
 function createSixteenGridContainer(gridSize) {
